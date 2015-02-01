627497a83805a5bedfa10b074652ba1610e33eb3
Diomidis Spinellis <dds@aueb.gr>
2015-02-01 01:30:31 -0800

    Create numbered field header file

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ else
 CXXFLAGS+=-O2
 endif
 
-all: qmcalc README.md header.tab
+all: qmcalc README.md header.tab header.txt
 
 HFILES=$(wildcard *.h)
 OFILES=CMetricsCalculator.o QualityMetrics.o
@@ -13,7 +13,7 @@ OFILES=CMetricsCalculator.o QualityMetrics.o
 UnitTests: UnitTests.cpp $(HFILES) $(OFILES)
 	$(CXX) $(LDFLAGS) UnitTests.cpp $(OFILES) -lcppunit -o $@
 
-test: UnitTests
+test: UnitTest
 	./UnitTests
 
 qmcalc: $(HFILES) $(OFILES) qmcalc.cpp
@@ -29,8 +29,13 @@ QualityMetricNames.h: QualityMetrics.h metric-names.sed
 README.md: make-readme.sh QualityMetrics.h QualityMetrics.cpp
 	sh make-readme.sh >$@
 
+# Tab-separated, ending with a newline
 header.tab: make-header.sh QualityMetrics.cpp
-	sh make-header.sh >$@
+	sh make-header.sh | tr ' \n' '\t\t' | sed 's/\t$$/\n/' >$@
+
+# Numbered lines
+header.txt: make-header.sh QualityMetrics.cpp
+	sh make-header.sh | tr ' \t' '\n\n' | cat -n >$@
 
 clean:
 	rm -f *.o *.exe qmcalc UnitTests
--- make-header.sh
+++ make-header.sh
@@ -1,50 +1,43 @@
 #!/bin/sh
 #
-# Generate the README.md file
+# Generate the header files as a list of tab or space-separated fields
 #
 # The lists are automatically derived from the source code
 #
 
-{
-	# Generate list of primary metrics
-	sed -n '
-	# Skip formatting style metrics
-	/q\.get_style_hint/d
-	# Print metrics variable
-	s/.*q\.get_\([^(]*\)().*/\1/p' QualityMetrics.cpp |
-	# Add further elements for descriptive metrics
-	while read metric
-	do
-		if grep -q "> $metric;" QualityMetrics.h
-		then
-			echo "${metric}_count ${metric}_min ${metric}_mean ${metric}_max ${metric}_sd"
-		else
-			echo $metric
-		fi
-	done |
-	# Convert _count metrics to the value measured
-	sed '
-	s/line_length_count/nline/
-	s/statement_nesting_count/nstatement/
-	s/halstead_count/nfunction/
-	s/cyclomatic_count/nfunction2/
-	s/identifier_length_count/nidentifier/
-	s/unique_identifier_length_count/nunique_identifier/
-	'
+# Generate list of primary metrics
+sed -n '
+# Skip formatting style metrics
+/q\.get_style_hint/d
+# Print metrics variable
+s/.*q\.get_\([^(]*\)().*/\1/p' QualityMetrics.cpp |
+# Add further elements for descriptive metrics
+while read metric
+do
+  if grep -q "> $metric;" QualityMetrics.h
+  then
+    echo "${metric}_count ${metric}_min ${metric}_mean ${metric}_max ${metric}_sd"
+  else
+    echo $metric
+  fi
+done |
+# Convert _count metrics to the value measured
+sed '
+s/line_length_count/nline/
+s/statement_nesting_count/nstatement/
+s/halstead_count/nfunction/
+s/cyclomatic_count/nfunction2/
+s/identifier_length_count/nidentifier/
+s/unique_identifier_length_count/nunique_identifier/
+'
 
-	# Generate list of style metrics
-	sed -n '/^\tenum StyleHint {/,/Add more elements here/{
-		/enum StyleHint/d
-		/Add more elements here/d
-		/^[ \t]*\/\//d
-		s/,$//
-		s/\t\t/N/
-		s/./\l&/g
-		p
-	}' QualityMetrics.h
-
-} |
-# Make the file tab-separated
-tr ' \n' '\t\t' |
-# Make trailing tab a newline
-sed 's/\t$/\n/'
+# Generate list of style metrics
+sed -n '/^\tenum StyleHint {/,/Add more elements here/{
+	/enum StyleHint/d
+	/Add more elements here/d
+	/^[ \t]*\/\//d
+	s/,$//
+	s/\t\t/N/
+	s/./\l&/g
+	p
+}' QualityMetrics.h
